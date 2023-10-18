81b07fecb2ab39581c7f19dfd9c18f2f720c8e37
Javidan18 <ayuyescavidan@gmail.com>
2023-10-18 09:44:36 +0300

    second

---
+++ nesnetabanliprogjava/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module nesnetabanliprogjava {
+}
---
+++ nesnetabanliprogjava/src/nesnetabanliprogjava/dersnotlari.java
@@ -0,0 +1,9 @@
+package nesnetabanliprogjava;
+
+public class dersnotlari {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
